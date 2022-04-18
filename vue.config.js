ed57d519a81037a5120428f9610f7201b187dcb3
CHOI DAE GEON <chleorjs37@gmail.com>
2022-04-18 21:50:41 +0900

    커밋

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 // 경로: 루트 디렉토리/vue.config.js
 const path = require('path');
+const VueAutoRoutingPlugin = require('vue-auto-routing/lib/webpack-plugin');
 // eslint-disable-next-line no-undef
 module.exports = {
 	lintOnSave: false,
@@ -10,4 +11,12 @@ module.exports = {
 			},
 		},
 	},
+	plugins:[
+		new VueAutoRoutingPlugin({
+			// Path to the directory that contains your page components.
+			pages: 'src/pages',
+			// A string that will be added to importing component path (default @/pages/).
+			importPrefix: '@/pages/'
+		})
+	]
 };
